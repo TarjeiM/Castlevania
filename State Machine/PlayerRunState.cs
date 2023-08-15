afd74616f21e472d07872b7b32b92d5f8125e6e1
TarjeiM <moland.tarjei@gmail.com>
2023-08-15 16:58:25 +0200

    moving boxes

--- State Machine/PlayerRunState.cs
+++ State Machine/PlayerRunState.cs
@@ -35,9 +35,11 @@ public class PlayerRunState : PlayerBaseState
             }
         }
     }
-    public override void OnCollisionEnter2D(PlayerStateManager player, Collision2D collision) {
-
+    public override void OnCollisionEnter2D(PlayerStateManager player, Collision2D collision) 
+    {
+      
     }
+
     public override void OnTriggerEnter2D(PlayerStateManager player, Collider2D other)
     {
         
